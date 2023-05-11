7d3a014150f62453515bdbaeafda789ab3079bed
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-05-11 05:59:06 -0700

    display arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+args = sys.argv
+if len(args) == 1:
+    print("{} arguments.".format(0))
+elif len(args) == 2:
+    print("{} argument:\n{}: {}".format(1, 1, args[1]))
+else:
+    print("{} arguments:".format(len(args) - 1))
+    i = 1
+    for arg in args[1:]:
+        print("{}: {}".format(i, arg))
+        i = i + 1
+    
