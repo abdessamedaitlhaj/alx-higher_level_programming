ee589ed4f61fbb5e65f844ab71c3ee8964fdb766
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-05-14 15:14:59 -0700

    fix

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    str = ""
+    for c in my_string:
+        if c != "c" and c != "C":
+            str += c
+    return str
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+<h2>Python - Data Structures: Lists, Tuples<h2>
