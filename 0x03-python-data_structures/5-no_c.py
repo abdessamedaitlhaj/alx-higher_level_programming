c625b43debba539b2ce3619671e0b4166346a867
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-05-15 06:15:33 -0700

    no c C

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    str = ""
-    for c in my_string:
-        if c != "c" and c != "C":
-            str += c
-    return str
+    if my_string:
+        str = ""
+        for c in my_string:
+            if c != "c" and c != "C":
+                str += c
+        return str
