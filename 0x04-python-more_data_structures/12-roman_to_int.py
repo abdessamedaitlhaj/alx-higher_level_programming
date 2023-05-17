9991fbd3737c15b1b358e453a5b1b33c690ee71f
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-05-17 16:25:40 -0700

    roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -13,18 +13,3 @@ def roman_to_int(roman_string):
     for c in roman_string:
         sum += roman[c]
     return sum
-
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "IX"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
