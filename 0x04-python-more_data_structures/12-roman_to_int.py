8968d41f7c0eda94e6c64b476d2d699f0c2d2456
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-05-17 16:27:07 -0700

    roman

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not roman_string:
-        return None
+        return 0
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     roman_sub = {'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
     sum = 0
