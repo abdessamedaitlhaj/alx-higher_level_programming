d403832dc16ea8ef08fb1e8ade9abfbc53c43694
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-05-31 13:21:57 -0700

    late

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         print("Inside result: ".format(a / b))
-    except:
+    except Exception:
         pass
     finally:
         return a / b if b != 0 else None
