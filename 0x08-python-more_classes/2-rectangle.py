66784f533e8e2fb50c465fd155fd5424973c39b2
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-06-05 15:31:00 -0700

    rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+class Rectangle:
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    def width(self):
+        return self.__width
+
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def height(self):
+        return self.__height
+
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if not value:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        return (self.__width + self.__height) * 2
+
+    width = property(width, width)
+    height = property(height, height)
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+<h2>Python - More Classes and Objects</h2>
