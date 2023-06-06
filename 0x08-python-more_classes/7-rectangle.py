37e11199a679e42473351f07075bad7e7b514a26
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-06-05 17:53:44 -0700

    rectangle

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+class Rectangle:
+
+    number_of_instances = 0
+    print_symbol = "#"
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+        Rectangle.number_of_instances += 1
+
+    def width(self):
+        return self.__width
+
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def height(self):
+        return self.__height
+
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if not value:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        return (self.__width + self.__height) * 2
+
+    def __str__(self):
+        if not self.__width and not self.__height:
+            return (str(self.print_symbol) * self.__width + "\n") * (self.__height - 1) + \
+                    str(self.print_symbol) * self.__width
+        else:
+            return ""
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
+
+    width = property(width, width)
+    height = property(height, height)
