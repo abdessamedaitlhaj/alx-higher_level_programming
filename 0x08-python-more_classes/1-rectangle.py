2d70c7ce43b38b6c7800021187ae86e6f83c8bc1
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-06-07 08:41:33 -0700

    rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
-"""
-Rectangle Module that defines a rectangle.
-"""
+""" Rectangle Module that defines a rectangle. """
 
 
 class Rectangle:
-    """
-    Rectangle class with attributes width and height.
-    """
+    """ Rectangle class with attributes width and height. """
     def __init__(self, width=0, height=0):
-        """Instantiation with optional width and height."""
+        """
+        Instantiation with optional width and height.
+
+        Parameters:
+        width(int): the width of the rectangle.
+        height(int): the height of the rectangle.
+        """
+
         self.__width = width
         self.__height = height
 
     @property
     def width(self):
-        """Property to retrieve width."""
+        """
+        Property to retrieve width.
+
+        Return:
+        int: the width of the rectangle.
+        """
+
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Property to set width."""
+        """ Property to set width. """
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -29,12 +39,19 @@ class Rectangle:
 
     @property
     def height(self):
-        """Property to retrieve height."""
+        """
+        Property to retrieve height.
+
+        Return:
+        int: the height of the rectangle.
+        """
+
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Property to set height"""
+        """ Property to set height """
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
