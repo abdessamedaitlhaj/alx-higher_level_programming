c115f3a01c55db1c9efef2fe81cb15399e2a5baf
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-06-10 03:14:58 -0700

    sqaure

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module that defines a square"""
+
+
+class Square:
+    """Square class that defines a square"""
+
+    def __init__(self, size=0):
+        """Initialization of the attribute size
+
+            Args:
+                size (int): the size of the square
+
+            Raises:
+                TypeError size must be an integer
+                ValueError size must be >= 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square
+
+            Return:
+                the area of the square
+        """
+        return self.__size * self.__size
