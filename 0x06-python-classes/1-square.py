cea621ab2d7bf00db6debe055117842747dfb78c
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-06-10 03:05:10 -0700

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""module that define a square"""
+
+
+class Square:
+    """Square class that defines a square"""
+
+    def __init__(self, size):
+        """Initialization of the attribute size
+
+            Args:
+                size : the size of the square
+        """
+        self.__size = size
