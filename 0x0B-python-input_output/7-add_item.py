3ea0856770e9f19f735438196038a65ea723f72b
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-06-13 05:29:22 -0700

    mandatory

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-"""
-Module that defines script that adds all arguments
-to a Python list, and then save them to a file
-"""
 import sys
 import json
 save_json = __import__("5-save_to_json_file")
