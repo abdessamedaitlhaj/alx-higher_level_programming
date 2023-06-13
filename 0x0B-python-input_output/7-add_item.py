873cb4fbd7fb27f046f61414f976eb78841bd40e
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-06-13 05:28:46 -0700

    mandatory

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,11 +8,10 @@ import json
 save_json = __import__("5-save_to_json_file")
 load_json = __import__("6-load_from_json_file")
 
+
 my_list = []
 
-
 my_list += load_json.load_from_json_file("add_item.json")
-
 
 for arg in sys.argv[1:]:
     my_list.append(arg)
