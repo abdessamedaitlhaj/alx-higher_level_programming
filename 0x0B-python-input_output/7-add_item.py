59f676ec3979587a81b558eb3ac71f0abd5175d1
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-06-13 05:31:09 -0700

    mandatory

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+Module that defines script that adds all arguments
+to a Python list, and then save them to a file
+"""
 import sys
 import json
 save_json = __import__("5-save_to_json_file")
@@ -7,9 +11,11 @@ load_json = __import__("6-load_from_json_file")
 
 my_list = []
 
+#load what's already in the json file
 my_list += load_json.load_from_json_file("add_item.json")
 
 for arg in sys.argv[1:]:
     my_list.append(arg)
 
+#save the list to json file
 save_json.save_to_json_file(my_list, "add_item.json")
