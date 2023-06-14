7887a2b8fedaf5db8c931a13a18bb83cbeaf868b
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-06-14 06:58:34 -0700

    mandatory

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module that defines a is_same_class function"""
+
+
+def is_same_class(obj, a_class):
+    """is obj an instance of a_class?
+
+        Return:
+            True or False
+    """
+    return isinstance(obj, a_class)
