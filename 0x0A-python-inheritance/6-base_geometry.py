e128d6c9bc15babc18055b214d9a2be50397d449
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-06-14 07:47:24 -0700

    mandatory

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that defines a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """BaseGeometry class that defines area() method"""
+    def area(self):
+        """Not defined yet!
+
+            Raises:
+                Exception area() is not implemented
+        """
+        raise Exception("area() is not implemented")
