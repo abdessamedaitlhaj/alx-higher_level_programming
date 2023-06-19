37aa176294ba0d1c17135949facf27d1faad3aae
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-06-19 12:32:15 -0700

    fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -43,8 +43,9 @@ class Square(Rectangle):
             Args:
                 args (str): list of args
         """
-        if args:
-            if len(args) >= 1:
+        if len(args) >= 1:
+            if type(args[0]) != int and args[0] is not None:
+                    raise TypeError("id must be an integer")
                 self.id = args[0]
             if len(args) >= 2:
                 self.width = args[1]
@@ -53,7 +54,7 @@ class Square(Rectangle):
                 self.x = args[3]
             if len(args) >= 4:
                 self.y = args[4]
-        if kwargs:
+        else:
             if "id" in kwargs:
                 self.id = kwargs['id']
             if "size" in kwargs:
