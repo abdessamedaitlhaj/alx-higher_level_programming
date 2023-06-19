6de0182aaa3e8da0e01cbeed5f1373384bae7529
AbdessammeD <aitlhaj.abdessamed@gmail.com>
2023-06-19 12:29:22 -0700

    fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -47,22 +47,22 @@ class Square(Rectangle):
             if len(args) >= 1:
                 self.id = args[0]
             if len(args) >= 2:
-                self.__width = args[1]
-                self.__height = args[1]
+                self.width = args[1]
+                self.height = args[1]
             if len(args) >= 3:
-                self.__x = args[3]
+                self.x = args[3]
             if len(args) >= 4:
-                self.__y = args[4]
+                self.y = args[4]
         if kwargs:
             if "id" in kwargs:
                 self.id = kwargs['id']
             if "size" in kwargs:
-                self.__width = kwargs['size']
-                self.__height = kwargs['size']
+                self.width = kwargs['size']
+                self.height = kwargs['size']
             if "x" in kwargs:
-                self.__x = kwargs['x']
+                self.x = kwargs['x']
             if "y" in kwargs:
-                self.__y = kwargs['y']
+                self.y = kwargs['y']
 
     def to_dictionary(self):
         """Returns the dictionary representation
