04f69be754e46c3617e5e934b1ecfc715c9033bb
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-07-12 09:12:12 -0700

    write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module that writes a string to a text file (UTF8) and 
+returns the number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """Write a text to a file and return number
+    of char written
+
+        Args:
+            filename(file): the file to write in
+            text(str): the content to write
+
+        Return:
+            the number of char written
+    """
+    with open(filename, "w+", encoding='utf-8') as f:
+        f.write(text)
+    return len(text)
