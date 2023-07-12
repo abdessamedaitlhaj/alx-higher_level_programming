f70723a14162a6ecf6c778bfd728d1756cd78961
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-07-12 09:02:34 -0700

    read and print a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """Module that reads a text file and prints it to stdout"""
 
+
 def read_file(filename=""):
+    """Open and print a file
+
+        Args:
+            filename(file): the file to be readed and printed
+    """
     with open(filename, "r", encoding='utf-8') as f:
         f.reads()
