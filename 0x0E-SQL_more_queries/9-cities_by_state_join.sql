b79c1f58b5323d6eb92b7e3629ecf9d50a984163
Abdessamed <aitlhaj.abdessamed@gmail.com>
2023-08-17 15:47:06 -0700

    SQL more queries

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- List all cities contained in the database hbtn_0d_usa
 SELECT cities.id, cities.name, states.name
-FROM states NATURAL JOIN cities ON cities.id = states.id;
+FROM states JOIN cities ON cities.id = states.id;
 ORDER BY cities.id;
