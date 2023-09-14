a7231e2e521eb2c8389201a09c2bece0d7c15146
abdessamed ait lhaj <aitlhaj.abdessamed@gmail.com>
2023-09-14 01:25:35 -0700

    args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-
-const argc = process.argv.length;
-
-if (argc === 2) {
-  console.log('No argument');
-} else if (argc === 3) {
-  console.log('Argument found');
+const args = process.argv;
+if (args.length == 2) {
+	console.log('No argument');
+}
+else if (args.length == 3) {
+	console.log('Argument found');
 } else {
-  console.log('Arguments found');
+	console.log('Arguments found');
 }
