db6589e6d2ff91bf9dc604aec197928f10b74370
abdessamed ait lhaj <aitlhaj.abdessamed@gmail.com>
2023-09-14 15:41:43 -0700

    add two integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,11 +5,7 @@ function add (a, b) {
 }
 
 const args = process.argv;
-if (args.length <= 3) {
-  console.log('NaN');
-} else {
-  const myInt1 = parseInt(args[2]);
-  const myInt2 = parseInt(args[3]);
-  if (isNaN(myInt1) || isNaN(myInt2)) console.log('NaN');
-  else console.log(add(args[2], args[3]));
-}
+const myInt1 = parseInt(args[2]);
+const myInt2 = parseInt(args[3]);
+if (isNaN(myInt1) || isNaN(myInt2)) console.log('NaN');
+else console.log(add(myInt1, myInt2));
