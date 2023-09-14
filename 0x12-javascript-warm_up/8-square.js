a18d635fb890915a44d02ba4e82e8bff4fc1f141
abdessamed ait lhaj <aitlhaj.abdessamed@gmail.com>
2023-09-14 08:13:05 -0700

    print square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,6 +4,7 @@ if (args.length <= 2) {
   console.log('Missing size');
 } else {
   const myInt = parseInt(args[2]);
+  if (isNaN(myInt)) console.log('Missing size');
   if (myInt > 0) {
     let row = '';
     for (let i = 0; i < myInt; i++) {
