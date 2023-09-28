f9e80fe2e71fedbff37f9871d2eb24ddd93fa3ff
abdessamed <aitlhaj.abdessamed@gmail.com>
2023-09-28 12:42:26 -0700

    networking

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,10 +3,13 @@
 
 def find_peak(list_of_integers):
     """Return the peak"""
+    if list_of_integers == []:
+        return None
     for i in enumerate(list_of_integers):
         curr = list_of_integers[i[0]]
         nex = list_of_integers[i[0] + 1]
-        bef = list_of_integers[i[0] - 1]
+        if list_of_integers[i[0] - 1]:
+            bef = list_of_integers[i[0] - 1]
         if i[0] == 0 and curr >= nex:
             return curr;
         elif curr >= bef and curr >= nex:
