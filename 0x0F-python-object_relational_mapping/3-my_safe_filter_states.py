e36e7bd8cf48cb0f33257ffe91877dd7501b286a
“abdessamed” <aitlhaj.abdessamed@gmail.com>
2023-11-19 09:11:07 -0800

    sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that lists all states with a name matches the argument
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    u = sys.argv[1]
+    p = sys.argv[2]
+    db = sys.argv[3]
+    name = sys.argv[4].partition("'")[0]
+
+    conn = MySQLdb.connect(host="localhost", user=u,
+                           passwd=p, db=db, port=3306)
+
+    cur = conn.cursor()
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(name)
+    cur.execute(query)
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    conn.close()
