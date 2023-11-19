bda3be903d758976b3f88cfc574bb8a5c426c4ac
“abdessamed” <aitlhaj.abdessamed@gmail.com>
2023-11-19 08:07:07 -0800

    select all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     p = sys.argv[2]
     db = sys.argv[3]
 
-    conn = MySQLdb.connect(host="127.0.0.1", user=u, passwd=p, db=db)
+    conn = MySQLdb.connect(host="localhost", user=u,
+                           passwd=p, db=db, port=3306)
 
     cur = conn.cursor()
     query = "SELECT * FROM states ORDER BY id"
