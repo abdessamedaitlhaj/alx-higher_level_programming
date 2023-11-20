349deec96753e4e7b4e66f2ca4a8e8f1944f2458
“abdessamed” <aitlhaj.abdessamed@gmail.com>
2023-11-19 17:33:55 -0800

    define state class with ORM

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Desfinition of the class State
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (create_engine)
+import sys
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name =  Column(String(128), nullable=False)
+
+
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+
+if __name__ == 'main':
+    main()
