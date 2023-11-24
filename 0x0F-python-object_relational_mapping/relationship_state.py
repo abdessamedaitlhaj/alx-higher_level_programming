6d6e8fb68a12eefb69a9e03f2312f2952e02d5e8
“abdessamed” <aitlhaj.abdessamed@gmail.com>
2023-11-24 13:22:52 -0800

    sql alchemy

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Desfinition of the class State
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Definition of class attributes"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='states', cascade='all, delete-orphan')
