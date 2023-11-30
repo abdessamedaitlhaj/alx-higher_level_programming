c742be7cb16a0ddcedb80eb4f44cfbcbeb9c3aa3
“abdessamedaitlhaj” <aitlhaj.abdessamed@gmail.com>
2023-11-30 15:35:50 -0800

    fix

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send POST request with variables
-curl -s "$1" -d "email=test@gmail.com&subject=I will always be here for PLD"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
