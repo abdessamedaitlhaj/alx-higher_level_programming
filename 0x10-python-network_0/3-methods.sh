683c43aab438dc9420d56c63f0608a5f486844c1
“abdessamedaitlhaj” <aitlhaj.abdessamed@gmail.com>
2023-11-30 15:25:11 -0800

    fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # list all methods within an HTPP response
-curl -sI "$1" | grep "Allow" | awk -F ":" '{print $2}' | tr -d " "
+curl -sI "$1" | grep "Allowed" | awk -F ":" '{print $2}' | tr -d " "
